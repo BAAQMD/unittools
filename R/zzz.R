04411e10e35cd1fe8b4fba7427d4db0c7805f28a
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-06-23 15:37:12 -0700

    Replace all calls to (deprecated) install_symbolic_unit with the recommended replacement: install_unit.
    David will need to review this checkin carefully as the parameters he was setting in install_symbolic_unit do not seem to have exact replacements in install_unit.

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,11 @@
+#' @importFrom units install_unit
 .onLoad <- function(libname, pkgname) {
 
-  install_unit <- function (u) {
-    units::install_symbolic_unit(u, warn = FALSE, dimensionless = FALSE)
-  }
-
-  install_unit("tput")
-  install_unit("person")
-  install_unit("death")
-  install_unit("cancer")
-  install_unit("veh")
+  units::install_unit("tput")
+  units::install_unit("person")
+  units::install_unit("death")
+  units::install_unit("cancer")
+  units::install_unit("veh")
 
   try(
     units::install_conversion_constant("VMT", "veh*mi", 1),
