f213aa69c852a8345e8e8ccdf55b1a90f4c8b29f
David Holstius <dholstius@baaqmd.gov>
2021-07-06 17:11:57 -0700

    narrow the conditions we'll pass silently on

--- R/try_install_unit.R
+++ R/try_install_unit.R
@@ -1,23 +1,42 @@
 #' try_install_unit
 #'
-#' Just like `units::install_unit()`, but re-raises error(s) as warning(s).
+#' Just like `units::install_unit()`, except that it does nothing if you try to "re-install" a unitless unit.
+#' (Unless you pass `verbose = TRUE`, in which case it will just output a message to that effect.)
 #'
 #' @param ... passed to [units::install_unit()]
 #'
+#' @importFrom stringr str_c str_trim
+#'
 #' @note The `units` package used to support `install_symbolic_unit(..., warn = TRUE)`.
 #' With that syntax, attempting to install an already-installed unit just led to a warning;
-#' it didn't throw an error. This is a way of recreating that behavior.
+#' it didn't throw an error.
 #'     There doesn't seem to be a function offered by `units` to check whether a symbolic
-#' unit is already installed. If there were, that would seem to be more prefereable than
-#' a blanket `tryCatch()`.
+#' unit is already installed. If there were, that would seem to be more preferable.
 #'
 #' @export
-try_install_unit <- function (...) {
+try_install_unit <- function (
+  symbol = character(0),
+  def = character(0),
+  name = character(0),
+  verbose = FALSE) {
+
+  msg <- function (...) if (isTRUE(verbose)) {
+    message("[try_install_unit] ", stringr::str_trim(paste0(...)))
+  }
+
+  handle_error <- function (e) {
+    err_msg <- as.character(e)
+    if (stringr::str_detect(err_msg, "already maps to existing but different unit")) {
+      if (def == "unitless") {
+        msg(e)
+        return(invisible(NULL)) # pass
+      }
+    }
+    stop(e)
+  }
 
   tryCatch(
-    units::install_unit(...),
-    error = function (e) {
-      warning(e)
-    })
+    units::install_unit(symbol = symbol, def = def, name = name),
+    error = handle_error)
 
 }
