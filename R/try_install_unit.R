78385b907f97919f6a0aedc0c49d5b57722adc82
David Holstius <david.holstius@gmail.com>
2022-04-05 09:53:28 -0700

    update try_install_unit()

--- R/try_install_unit.R
+++ R/try_install_unit.R
@@ -1,11 +1,11 @@
 #' try_install_unit
 #'
-#' Just like `units::install_unit()`, except that it does nothing if you try to "re-install" a unitless unit.
+#' Just like `units::install_unit()`, except that it does nothing if you try to "re-install" an existing unit.
 #' (Unless you pass `verbose = TRUE`, in which case it will just output a message to that effect.)
 #'
 #' @param ... passed to [units::install_unit()]
 #'
-#' @importFrom stringr str_c str_trim
+#' @importFrom stringr str_detect
 #'
 #' @note The `units` package used to support `install_symbolic_unit(..., warn = TRUE)`.
 #' With that syntax, attempting to install an already-installed unit just led to a warning;
@@ -21,16 +21,14 @@ try_install_unit <- function (
   verbose = FALSE) {
 
   msg <- function (...) if (isTRUE(verbose)) {
-    message("[try_install_unit] ", stringr::str_trim(paste0(...)))
+    message("[try_install_unit] ", paste0(...))
   }
 
   handle_error <- function (e) {
     err_msg <- as.character(e)
     if (stringr::str_detect(err_msg, "already maps to existing but different unit")) {
-      if (def == "unitless") {
-        msg(e)
-        return(invisible(NULL)) # pass
-      }
+      msg(e)
+      return(invisible(NULL)) # pass
     }
     stop(e)
   }
