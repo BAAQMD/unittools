d7cd533520832101fee73bbea44ab34a75e336f8
David Holstius <david.holstius@gmail.com>
2022-04-05 09:47:35 -0700

    improve Rd

--- R/has_units.R
+++ R/has_units.R
@@ -1,12 +1,14 @@
 #' has_units
 #'
-#' @param x
-#' @param u
+#' Returns `TRUE` if `x` is either (a) unitless, or is not of class "units".
+#' If `x` is of class "units", can test whether the units are equal to `u`.
 #'
-#' @return
+#' @param x (vector) to be tested
+#' @param u (optional) expected units
+#'
+#' @return `TRUE` or `FALSE`
+#'
 #' @export
-#'
-#' @examples
 has_units <- function (x, u = NULL) {
   if (isFALSE(is_units(x))) return(FALSE)
   if (isTRUE(is.null(u))) return(is_units(x))
