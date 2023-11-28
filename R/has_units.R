143e154a468fb8d84197928aa3fc78fd8c0f1deb
David Holstius <david.holstius@gmail.com>
2023-11-28 12:34:01 -0800

    refactor has_units() to rely on get_units()

--- R/has_units.R
+++ R/has_units.R
@@ -10,9 +10,12 @@
 #'
 #' @export
 has_units <- function (x, u = NULL) {
-  if (isFALSE(is_units(x))) return(FALSE)
-  if (isTRUE(is.null(u))) return(is_units(x))
-  x_units <- units::deparse_unit(x)
-  u_units <- units::deparse_unit(units::as_units(1, u))
-  return(x_units == u_units)
+  ux <- get_units(x)
+  if (is.null(ux)) {
+    return(FALSE)
+  } else if (!is.null(u)) {
+    return (ux == get_units(set_units(1, u, mode = "character")))
+  } else {
+    return(TRUE)
+  }
 }
--- tests/testthat/test-has_units.R
+++ tests/testthat/test-has_units.R
@@ -1,9 +1,9 @@
 test_that("has_units() works", {
 
-  x <- set_units(1:3, "ton/yr")
+  x <- set_units(1:3, "ton/d")
 
   expect_true(has_units(x))
-  expect_true(has_units(x, "ton/yr"))
+  expect_true(has_units(x, "ton/day"))
   expect_false(has_units(x, "g/s"))
 
   expect_false(has_units(1:3))
